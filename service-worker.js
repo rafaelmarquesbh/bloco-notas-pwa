0d970c2ef6a1b1b3aca4d262b88458ac204e1c34
rafaelmarquesbh <rafaelmarquesbh@gmail.com>
2026-09-03 14:50:36 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "notas-v1716";
+const CACHE_NAME = "notas-v20";
 const APP_ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./config.js", "./manifest.json", "./favicon.svg"];
 const SUPABASE_JS = "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
 
